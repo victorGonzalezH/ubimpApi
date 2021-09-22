e917b2b54afa1003addb21857b604621776956b1
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2021-09-22 12:09:35 -0500

    restricting get vehicles by group with guard

--- sourceCode/apps/ubimp.api/src/controllers/vehicles/vehicles.controller.ts
+++ sourceCode/apps/ubimp.api/src/controllers/vehicles/vehicles.controller.ts
@@ -1,5 +1,6 @@
-import { BadRequestException, Controller, Get, Param, Post, Query } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Query, UseGuards } from '@nestjs/common';
 import { VehicleGroupDto } from 'uba/ubimp.application/dataTransferObjects/vehicleGroup.dto';
+import { JwtAuthGuard } from 'uba/ubimp.application/services/auth/auth.guard';
 import { VehiclesApplication } from 'uba/ubimp.application/services/vehicle/vehicle.service';
 
 @Controller('vehicles')
@@ -9,6 +10,7 @@ export class VehiclesController {
         
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('groups')
     async get(@Query() query): Promise<VehicleGroupDto[]> {
       if(query.userid !== undefined || query.userId !== undefined)
