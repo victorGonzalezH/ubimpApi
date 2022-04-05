a86eec41c1fe8041bccb7564c1a6de1d9f93263b
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2022-04-04 23:16:46 -0500

    Added brand, brandModel and RefreshToken Schemas as providers

--- sourceCode/libs/ubimp.infrastructure/src/ubimp.infrastructure.module.ts
+++ sourceCode/libs/ubimp.infrastructure/src/ubimp.infrastructure.module.ts
@@ -14,12 +14,21 @@ import { VehiclesRepository } from './persistence/repositories/vehicle-repositor
 import { VehicleGroupRepository } from './persistence/repositories/vehicle-group-repository/vehicle-group-repository.service';
 import { VehicleSchema } from './persistence/schemas/vehicle.schema';
 import { VehicleGroupSchema } from './persistence/schemas/vehicle-group.schema';
+import { BrandsRepository } from './persistence/repositories/brands-repository/brands-repository.service';
+import { BrandSchema } from './persistence/schemas/brand.schema';
+import { BrandModelSchema } from './persistence/schemas/brand-model.schema';
+import { UserPrefRepositoryService } from './persistence/repositories/user-pref-repository/user-pref-repository.service';
+import { RefreshTokenRepository } from './persistence/repositories/refresh-token-repository/refresh-token-repository.service';
+import { RefreshTokenSchema } from './persistence/schemas/refresh-token.schema';
 
 @Module({
   providers: [UbimpInfrastructureService, LanguageRepository, MessagesRepository,
-    CountriesRepository, DevicesRepository, VehiclesRepository, VehicleGroupRepository],
+    CountriesRepository, DevicesRepository, VehiclesRepository, VehicleGroupRepository,
+    BrandsRepository, UserPrefRepositoryService, RefreshTokenRepository],
   exports: [UbimpInfrastructureService],
   imports: [MongooseModule.forFeature([
+    //We must dechlae the schemas here due that they are injected into
+    //the repositories constructors
     { name: 'Message', schema: MessageSchema },
     { name: 'Messages', schema: MessagesSchema },
     { name: 'Language', schema: LanguageSchema },
@@ -27,6 +36,9 @@ import { VehicleGroupSchema } from './persistence/schemas/vehicle-group.schema';
     { name: 'Device', schema: DeviceSchema },
     { name: 'Vehicle', schema: VehicleSchema },
     { name: 'VehicleGroup', schema: VehicleGroupSchema },
+    { name: 'Brand', schema: BrandSchema },
+    { name: 'BrandModel', schema: BrandModelSchema },
+    {name: 'RefreshToken', schema: RefreshTokenSchema},
   ]) ],
 })
 export class UbimpInfrastructureModule {}
