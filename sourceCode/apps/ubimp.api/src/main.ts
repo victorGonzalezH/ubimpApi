9b3d5fff30e028408090c153d8c46a33364016c1
Victor Gonzalez <victor.gonzalez@metricsfab.com>
2022-06-15 23:42:47 -0500

    Changes for refresh token support and new web url ssl

--- sourceCode/apps/ubimp.api/src/main.ts
+++ sourceCode/apps/ubimp.api/src/main.ts
@@ -3,6 +3,7 @@ import { NestFactory } from '@nestjs/core';
 import { IoAdapter } from '@nestjs/platform-socket.io';
 import { UbimpApiModule } from './ubimp.api.module';
 import * as fs from 'fs';
+import * as cookieParser from 'cookie-parser';
 
 import { join } from 'path';
 import { AppConfigService } from 'uba/ubimp.application/config/appConfig.service';
@@ -16,29 +17,43 @@ async function bootstrap() {
   };
 
   const app = await NestFactory.create(UbimpApiModule, {
-    httpsOptions,
+    httpsOptions
   });
+
+  
   // app.useWebSocketAdapter(new IoAdapter(app));
   const appConfigService = app.get<AppConfigService>(AppConfigService);
+  
+  //Enabling cookie parser
+  app.use(cookieParser());
 
   app.enableCors({
-    origin: true,
+    origin: 'https://localhost:4200',
     credentials: true,
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
-    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization'});
+    // allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept, Authorization, Cookie, Set-Cookie, Access-Control-Allow-Credentials, Allow-Origin-With-Credentials',
+    
+  });
 
-  app.use((req, res, next) => {
-      res.header('Access-Control-Allow-Origin', '*');
-      res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
-      next();
-    });
+   
+  // app.use((req, res, next) => {
+  //     res.header('Access-Control-Allow-Origin', '*');
+  //     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
+  //     next();
+  //   });
+
+  
 
   app.useGlobalPipes(new ValidationPipe( {  disableErrorMessages : appConfigService.getDisableErrorMessages() } ));
   app.connectMicroservice({ transport: appConfigService.getMicroserviceProtocol(), options: {  port: appConfigService.getMicroservicePort() } } );
 
-  
+  //Starting the micorservice
   await app.startAllMicroservicesAsync();
-  await app.listen(appConfigService.getWebPort());
+  await app.listen(appConfigService.getApiPort());
 }
 
+
+/**
+ * Bootstraping the application
+ */
 bootstrap();
